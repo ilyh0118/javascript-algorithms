877d45981c10d18d33082c627d50a796435cf610
Ilyh Kalinin <ilyapochta0118@mail.ru>
2022-04-14 09:52:22 +0300

    2_tasks

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,9 +11,24 @@
 */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
+    for (let i = 1; i <= num; i++) {
+        if (!(i % 3) && !(i % 5)) {
+            console.log('fizzbuzz');
+        }
+        else if(!(i % 3)) {
+            console.log('fizz');
+        }
+        else if(!(i % 5)) {
+            console.log('buzz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+    return 'Функция завершила выполнение';
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fizzBuzz(15));
+console.log(fizzBuzz(90));
+//console.log(fizzBuzz(15));
