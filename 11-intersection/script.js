005b130b05f46b1b83783328fd361cf4f848e7dd
Ilyh Kalinin <ilyapochta0118@mail.ru>
2022-04-18 21:13:16 +0300

    10, 11 tasks

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -8,10 +8,12 @@
 */
 
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+    const interArr =  arr1.filter(elemArr1 => arr2.some(elemArr2 => elemArr2 === elemArr1));
+    return interArr.filter((elemInterArr,pos) => interArr.lastIndexOf(elemInterArr) === pos);
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(intersection([1, 5, 4, 2], [8, 91, 4, 1, 3])); // [4, 1]
+console.log(intersection([1, 1, 5, 4, 2], [8, 91, 4, 1, 1, 1, 3])); // [4, 1]
 console.log(intersection([1, 5, 4, 2], [7, 12])); // []
+console.log(intersection([1, 5, 5, 1, 2, 2, 4, 2], [7, 2, 2, 2, 1, 5, 5, 4, 12])); // []
